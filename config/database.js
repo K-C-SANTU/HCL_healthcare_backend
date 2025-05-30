34074bc65510fc4f47fd68a0c999ec9589eccfe9
KC SANTOSH <kcsantoshprojects@gmail.com>
2025-05-30 15:24:22 +0530

    auto deployments

--- config/database.js
+++ config/database.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      process.env.MONGODB_URI || "mongodb://localhost:27017/hcl_healthcare",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      process.env.MONGODB_URI || "mongodb://localhost:27017/hcl_healthcare"
     );
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
